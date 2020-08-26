59771343b79ef7f54afdcdb59e9f6d1fe5e704b5
vasu9691 <68852287+vasu9691@users.noreply.github.com>
2020-08-25 22:52:43 -0700

    added files

---
+++ script.js
@@ -0,0 +1,28 @@
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 3000;	// Time Between Switch
+
+// Image List
+images[0] = "images/pig.jpg";
+images[1] = "images/food.jpg";
+images[2] = "images/people.jpg";
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++;
+	} else {
+		// Reset Back To O
+		i = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
